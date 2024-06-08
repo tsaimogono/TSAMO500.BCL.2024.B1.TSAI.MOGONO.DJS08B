1039e6d986853f194bc70dbb40802ef86a02121d
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-09 00:12:59 +0200

    added a serber file and imported to index.jsx

--- index.jsx
+++ index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 
+import "./server"
 function App() {
   return (
     <BrowserRouter>
