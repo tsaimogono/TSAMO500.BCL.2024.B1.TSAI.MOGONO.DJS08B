54295eb3518762df6753ecb61165756282470a2d
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-09 01:39:37 +0200

    HostLayout component sets up a navigation menu specifically for a "Host" section

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,11 +1,14 @@
 import React from "react"
-import { Outlet } from "react-router-dom"
-import Header from "./Header"
+import { Link, Outlet } from "react-router-dom"
 
-export default function Layout() {
+export default function HostLayout() {
     return (
         <>
-            <Header />
+            <nav className="host-nav">
+                <Link to="/host">Dashboard</Link>
+                <Link to="/host/income">Income</Link>
+                <Link to="/host/reviews">Reviews</Link>
+            </nav>
             <Outlet />
         </>
     )
