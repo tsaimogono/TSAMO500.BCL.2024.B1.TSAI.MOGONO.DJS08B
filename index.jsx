c0f2143fc2585588f925859cd314d4b5cfe34a7f
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-09 00:30:24 +0200

    added vansdetail and modified the css style

--- index.jsx
+++ index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+
 
 import "./server"
 function App() {
