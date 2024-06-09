ee47b3ef049f7b0a9e05748fa35ae5cdedea1031
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-09 23:52:51 +0200

    AuthRequired Function Component: The AuthRequired component is declared as a functional component using the arrow function syntax (() => { ... }).
    Authentication Check: Inside the component, there's a check for authentication. In this case, the authenticated variable is set to false, indicating that the user is not authenticated.
    
    Return JSX: If the user is not authenticated (authenticated is false), the component returns a Navigate component. This causes a redirect to the login page ("/login") with state data containing a message indicating that the user must log in first.
    
    Return Outlet: If the user is authenticated, the component returns an Outlet. The Outlet component is used to render child routes, allowing access to the protected content

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,9 +1,10 @@
 import React from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
-
+    const location = useLocation()
+    
     function handleSubmit(e) {
         e.preventDefault()
         console.log(loginFormData)
@@ -16,9 +17,14 @@ export default function Login() {
             [name]: value
         }))
     }
-
+    
+ 
     return (
         <div className="login-container">
+            {
+                location.state?.message &&
+                <h3 className="login-first">{location.state.message}</h3>
+            }
             <h1>Sign in to your account</h1>
             <form onSubmit={handleSubmit} className="login-form">
                 <input
