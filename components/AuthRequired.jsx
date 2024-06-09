fc3f185c6cc845a03338926e69548c09ab731469
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-10 00:05:53 +0200

    React Import: The component imports React from the React library. This is necessary for creating React components.
    React Icons Import: The component imports the BsStarFill icon from "react-icons/bs". This icon is used to represent star ratings in the reviews

--- components/AuthRequired.jsx
+++ components/AuthRequired.jsx
@@ -1,14 +1,19 @@
 import React from "react"
-import { Outlet, Navigate } from "react-router-dom"
+import { Outlet, Navigate, useLocation } from "react-router-dom"
 
 export default function AuthRequired() {
-    const authenticated = false
+    const isLoggedIn = localStorage.getItem("loggedin")
+    const location = useLocation()
     
-    if (!authenticated) {
+    if (!isLoggedIn) {
         return (
             <Navigate 
                 to="/login" 
-                state={{message: "You must log in first"}} 
+                state={{
+                    message: "You must log in first",
+                    from: location.pathname
+                }} 
+                replace
             />)
     }
     return <Outlet />
