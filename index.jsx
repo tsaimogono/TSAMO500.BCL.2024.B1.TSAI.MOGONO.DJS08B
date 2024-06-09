8645bf0dc368b61a9bbf649574c8a5fd8ce58c49
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-09 23:26:21 +0200

    Token Storage: Storing tokens in localStorage is a simple method for this example. For more secure implementations, consider using HTTP-only cookies. Authentication State: For a more comprehensive application, you would use context or state management libraries (e.g., Redux) to manage authentication state across your app. Navigation: useNavigate from react-router-dom is used to redirect the user upon successful login.

--- index.jsx
+++ index.jsx
@@ -17,20 +17,10 @@ import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import NotFound from "./pages/NotFound"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
-import AuthRequired from "./components/AuthRequired"
 
 import "./server"
 
 function App() {
-  /**
-   * Challenge: Create the AuthRequired Layout Route to protect
-   * all the /host routes.
-   * 
-   * For now, just use `const authenticated = false`
-   * to determine the authenticated status of the user, and
-   * either send them to the /login route, or render the Outlet
-   */
-
   return (
     <BrowserRouter>
       <Routes>
@@ -44,20 +34,17 @@ function App() {
             element={<Login />}
           />
 
-          <Route element={<AuthRequired />}>
-            <Route path="host" element={<HostLayout />}>
-              <Route index element={<Dashboard />} />
-              <Route path="income" element={<Income />} />
-              <Route path="reviews" element={<Reviews />} />
-              <Route path="vans" element={<HostVans />} />
-              <Route path="vans/:id" element={<HostVanDetail />}>
-                <Route index element={<HostVanInfo />} />
-                <Route path="pricing" element={<HostVanPricing />} />
-                <Route path="photos" element={<HostVanPhotos />} />
-              </Route>
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
-
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
